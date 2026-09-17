33f83576cbb63c3d0df4ac5f6b07a446585ce27d
wadiwalarahenuma007-oss <wadiwalarahenuma007@gmail.com>
2026-09-17 22:53:22 +0530

    Fix production authentication cookies

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -108,8 +108,8 @@ const loginUser = async (req, res) => {
 const logoutUser = (req, res) => {
     res.cookie("token", "", {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         expires: new Date(0),
     });
     res.status(200).json({ success: true, message: "Logout successful" });
--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -11,8 +11,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
